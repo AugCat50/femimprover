f95ecc955fa7cbee14b66ec8011fc04b2a35dece
AugCat50 <augcat50@gmail.com>
2025-02-21 04:29:17 +0200

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+
+const title = document.createElement('h1');
+title.textContent = 'Laravel 4| Vit.re';
+document.body.appendChild(title);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document1</title>
+
+    @vite(['resourses/css/app.css', 'resourses/js/app.js'])
+</head>
+
+<body>
+    <h1>Laravel \\ Vite11</h1>
+</body>
+
+</html>
